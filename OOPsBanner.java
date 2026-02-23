edb886aa04cf55e3cb9ff295a9ddc2360b14fd6d
Grithanyaa <ga9876@srmist.edu.in>
2026-02-23 20:38:18 +0530

    Saving and updating code

--- OOPsBanner.java
+++ OOPsBanner.java
@@ -1,5 +1,11 @@
 public class OOPsBanner {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(String.join("", "  *****  ", "  *****  ", " ********", " ********"));
+        System.out.println(String.join("", " **   ** ", " **   ** ", " **    **", "**       "));
+        System.out.println(String.join("", "**     **", "**     **", " **    **", "**       "));
+        System.out.println(String.join("", "**     **", "**     **", " ********", " ********"));
+        System.out.println(String.join("", "**     **", "**     **", " **      ", "       **"));
+        System.out.println(String.join("", " **   ** ", " **   ** ", " **      ", "       **"));
+        System.out.println(String.join("", "  *****  ", "  *****  ", " **      ", "******** "));
     }
 }
